551d59f9f64f8328e91f9fe47a5c33e0cea7dd01
Marcos <marcosviniciohd@hotmail.com>
2023-06-19 17:15:28 -0300

    Aula sobre Modulos

---
+++ src/app/cursos/cursos-detalhe/cursos-detalhe.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-cursos-detalhe',
+  templateUrl: './cursos-detalhe.component.html',
+  styleUrls: ['./cursos-detalhe.component.css']
+})
+export class CursosDetalheComponent {
+
+}
